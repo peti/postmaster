25e59261142687eed97c051114efe235a6a9915e
Peter Simons <simons@cryp.tyo>
2005-06-23 11:31:18 +0000

    imported changes from public branch back

--- Postmaster.hs
+++ Postmaster.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS -fglasgow-exts #-}
 {- |
    Module      :  Postmaster
-   Copyright   :  (c) 2005-02-13 by Peter Simons
+   Copyright   :  (c) 2005-06-23 by Peter Simons
    License     :  GPL2
 
    Maintainer  :  simons@cryp.to
