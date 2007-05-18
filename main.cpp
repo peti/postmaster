7653713874968c500254b7acde8ca8058c20bc32
Peter Simons <simons@cryp.to>
2007-05-19 01:31:57 +0200

    Imported main() from mini-httpd.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001-2007 Peter Simons <simons@cryp.to>
+ * Copyright (c) 2007 Peter Simons <simons@cryp.to>
  *
  * This software is provided 'as-is', without any express or
  * implied warranty. In no event will the authors be held liable
@@ -10,165 +10,171 @@
  * provided the copyright notice and this notice are preserved.
  */
 
-#include <csignal>
-#include <boost/test/prg_exec_monitor.hpp>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/thread.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/compatibility/cpp_c_headers/csignal>
+#include <boost/program_options.hpp>
+#include <boost/test/included/prg_exec_monitor.hpp>
+#include <sanity/system-error.hpp>
+#include <iostream>
 
-using boost::asio::ip::tcp;
+#define TRACE(msg) std::cerr << "trace: [" <<__func__ << "] " << msg << std::endl
+#define INFO(msg)  std::cerr << "info: " << msg << std::endl
 
-class session
+#ifndef PACKAGE_NAME
+static char const PACKAGE_NAME[]    = "postmaster";
+#endif
+#ifndef PACKAGE_VERSION
+static char const PACKAGE_VERSION[] = "2007-05-10";
+#endif
+
+typedef boost::asio::io_service io_service;
+static boost::scoped_ptr<io_service> the_io_service;
+
+static void start_service()
 {
-public:
-  session(boost::asio::io_service& io_service)
-    : socket_(io_service)
-  {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-  }
-
-  ~session()
-  {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-  }
-
-  tcp::socket& socket()
-  {
-    return socket_;
-  }
-
-  void start()
-  {
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        boost::bind(&session::handle_read, this,
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-  }
-
-  void handle_read(const boost::system::error_code& error,
-      size_t bytes_transferred)
-  {
-    if (!error)
-    {
-      boost::asio::async_write(socket_,
-          boost::asio::buffer(data_, bytes_transferred),
-          boost::bind(&session::handle_write, this,
-            boost::asio::placeholders::error));
-    }
-    else
-    {
-      delete this;
-    }
-  }
-
-  void handle_write(const boost::system::error_code& error)
-  {
-    if (!error)
-    {
-      socket_.async_read_some(boost::asio::buffer(data_, max_length),
-          boost::bind(&session::handle_read, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
-    }
-    else
-    {
-      delete this;
-    }
-  }
-
-private:
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
-};
-
-class server
-{
-public:
-  server(boost::asio::io_service& io_service, short port)
-    : io_service_(io_service),
-      acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
-  {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    session* new_session = new session(io_service_);
-    acceptor_.async_accept(new_session->socket(),
-        boost::bind(&server::handle_accept, this, new_session,
-          boost::asio::placeholders::error));
-  }
-
-  ~server()
-  {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-  }
-
-  void handle_accept(session* new_session,
-      const boost::system::error_code& error)
-  {
-    if (!error)
-    {
-      new_session->start();
-      new_session = new session(io_service_);
-      acceptor_.async_accept(new_session->socket(),
-          boost::bind(&server::handle_accept, this, new_session,
-            boost::asio::placeholders::error));
-    }
-    else
-    {
-      delete new_session;
-    }
-  }
-
-private:
-  boost::asio::io_service& io_service_;
-  tcp::acceptor acceptor_;
-};
-
-static boost::asio::io_service & get_io_service()
-{
-  static boost::asio::io_service ios;
-  return ios;
+  the_io_service->run();
 }
 
-static void stop_io_service()
+static void stop_service()
 {
-  get_io_service().stop();
+  the_io_service->stop();
 }
 
-static void run_io_service()
-{
-  get_io_service().run();
-}
-
-int cpp_main(int argc, char * argv[])
+int cpp_main(int argc, char ** argv)
 {
   using namespace std;
-
-  // Make sure usage is correct.
-  if (argc != 2)
-  {
-    std::cerr << "Usage: async_tcp_echo_server <port>\n";
-    return 1;
-  }
-
-  // Setup the system.
-
-  ios::sync_with_stdio(false);
-  signal(SIGTERM, reinterpret_cast<sighandler_t>(&stop_io_service));
-  signal(SIGINT,  reinterpret_cast<sighandler_t>(&stop_io_service));
-  signal(SIGHUP,  reinterpret_cast<sighandler_t>(&stop_io_service));
-  signal(SIGQUIT, reinterpret_cast<sighandler_t>(&stop_io_service));
+  namespace po = boost::program_options;
+  //
+  // Ignore relevant signals to avoid race conditions.
+  //
+  signal(SIGTERM, SIG_IGN);
+  signal(SIGINT,  SIG_IGN);
+  signal(SIGHUP,  SIG_IGN);
+  signal(SIGQUIT, SIG_IGN);
   signal(SIGPIPE, SIG_IGN);
+  //
+  // Seed the random number generator.
+  //
+  srand(time(0));
+  //
+  // stdout/stderr are accessed only through the C++ streams API.
+  //
+  ios::sync_with_stdio(false);
+  //
+  // Parse the command line: Generic Process and Daemon Options
+  //
+  vector<string>        listen_addrs;
+  po::options_description meta_opts("Administrative Options");
+  meta_opts.add_options()
+    ( "help,h",                                                         "produce help message and exit" )
+    ( "version,v",                                                      "show program version and exit" )
+    ( "no-detach,D",                                                    "don't run in the background" )
+    ( "listen",         po::value< vector<string> >(&listen_addrs),     "listen on address:port"  )
+    ;
+  po::positional_options_description pos_opts;
+  pos_opts.add("listen", -1);
+  //
+  // Parse the command line: HTTP Page Delivery and Logging
+  //
+  string                hostname;
+  po::options_description httpd_opts("SMTP Daemon Configuration");
+  httpd_opts.add_options()
+    ( "hostname",       po::value<string>(&hostname),                   "hostname to use in HELO response")
+    ;
+  //
+  // Run command line parser. Obvious errors are thrown as exceptions.
+  //
+  po::options_description opts
+    ( string("Commandline Interface ") + PACKAGE_NAME + " " + PACKAGE_VERSION)
+    ;
+  opts.add(meta_opts).add(httpd_opts);
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(opts).positional(pos_opts).run(), vm);
+  po::notify(vm);
+  //
+  // Sanity checking ...
+  //
+  if (vm.count("help"))          { cout << opts << endl;                                     return 0; }
+  if (vm.count("version"))       { cout << PACKAGE_NAME << " " << PACKAGE_VERSION << endl;   return 0; }
+  if (listen_addrs.empty())      { cout << "no listen addresses configured" << endl;         return 1; }
+  bool const detach( !vm.count("no-detach") );
+  //
+  // Setup the system and log configuration.
+  //
+  the_io_service.reset(new io_service);
+  INFO(  PACKAGE_NAME << " version " << PACKAGE_VERSION
+      << " running " << (detach ? "as daemon" : "on current tty")
+      );
+  //
+  // Configure TCP listeners.
+  //
+  using namespace boost::asio::ip;
+  typedef boost::shared_ptr<tcp::acceptor>      shared_acceptor;
+  typedef vector<shared_acceptor>               acceptor_array;
+  acceptor_array                                acceptors;
+  for (vector<string>::iterator i( listen_addrs.begin() ); i != listen_addrs.end(); ++i)
+  {
+    tcp::endpoint addr;
+    string::size_type k( i->rfind(':') );
+    switch (k)
+    {
+      case string::npos:
+        TRACE("parse TCP port '" << *i << "'");
+        addr = tcp::endpoint(tcp::v6(), atoi(i->c_str()));
+        break;
 
-  // Start the server.
+      case 0u:
+        TRACE("parse any host, TCP port '" << *i << "'");
+        addr = tcp::endpoint(tcp::v6(), atoi(i->c_str() + 1u));
+        break;
 
-  server s(get_io_service(), atoi(argv[1]));
-  boost::thread t1(&run_io_service);
-  boost::thread t2(&run_io_service);
-  boost::thread t3(&run_io_service);
-  get_io_service().run();
-  t1.join(); t2.join(); t3.join();
-
-  // Shut down gracefully.
-
+      default:
+        TRACE(  "parse host '" << i->substr(0, k)
+             << "', port '"    << i->substr(k + 1u)
+             << "'"
+             );
+        addr = tcp::endpoint( address::from_string(i->substr(0, k))
+                            , atoi(i->substr(k + 1u).c_str())
+                            );
+    }
+    INFO("listen on network address " << addr);
+    shared_acceptor const acc( new tcp::acceptor(*the_io_service, addr) );
+    acceptors.push_back(acc);
+  }
+  //
+  // Detach from terminal.
+  //
+  if (detach)
+  {
+    switch (fork())
+    {
+      case -1:
+        throw system_error("fork() failed");
+      case 0:
+        setsid();
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+        break;
+      default:
+        return 0;
+    }
+  }
+  //
+  // Run the server.
+  //
+  signal(SIGTERM, reinterpret_cast<sighandler_t>(&stop_service));
+  signal(SIGINT,  reinterpret_cast<sighandler_t>(&stop_service));
+  signal(SIGHUP,  reinterpret_cast<sighandler_t>(&stop_service));
+  signal(SIGQUIT, reinterpret_cast<sighandler_t>(&stop_service));
+  start_service();
+  //
+  // Terminate gracefully.
+  //
+  INFO("shutting down");
+  acceptors.clear();
+  the_io_service.reset();
   return 0;
 }
