e0f9d1e6d8a9373b9f1f328b4659e13865105111
Peter Simons <simons@cryp.to>
2007-11-18 19:23:26 +0100

    Split monolithic scheduler class into core and scheduler. Added system_socket class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <boost/noncopyable.hpp>
 #include <boost/function/function0.hpp>
+#include <boost/function/function1.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/system/system_error.hpp>
 #include <boost/compatibility/cpp_c_headers/cstddef>
@@ -25,7 +26,7 @@ namespace postmaster
     using std::time_t;
     typedef unsigned int second_t;
 
-    // Errors
+    // Popular system error types.
 
     using boost::system::error_code;
     using boost::system::errno_ecat;
@@ -33,7 +34,7 @@ namespace postmaster
 
     // Co-operative multi-tasking scheduler.
 
-    class scheduler : private boost::noncopyable
+    class core : private boost::noncopyable
     {
     public:
       typedef boost::function0<void>                    task;
@@ -44,21 +45,14 @@ namespace postmaster
 
     public:
       typedef task_queue_entry                          task_id;
-      typedef int                                       socket_id;
 
-      explicit scheduler(int size_hint = 512u) : _epoll_fd( epoll_create(size_hint) )
-      {
-        if (_epoll_fd < 0) throw system_error(errno, errno_ecat, "epoll_create(2) failed");
-        update();
-      }
+      core() { update(); }
 
-      ~scheduler() { ::close(_epoll_fd); }
-
-      task_id schedule(task const & t, second_t delay = 0u)
+      task_id schedule(task const & t, second_t from_now = 0u)
       {
         BOOST_ASSERT(t);
-        if (delay > 0) update();
-        task_queue::iterator const i( _tasks.insert(std::make_pair(_now + delay, t)) );
+        if (from_now > 0) update();
+        task_queue::iterator const i( _tasks.insert(std::make_pair(_now + from_now, t)) );
         return std::make_pair(i->first, i);
       }
 
@@ -67,34 +61,51 @@ namespace postmaster
         if (tid.first > _now) _tasks.erase(tid.second);
       }
 
+      second_t run()
+      {
+        while (!_tasks.empty())
+        {
+          task_queue::iterator const i( _tasks.begin() );
+          if (i->first > _now)
+          {
+            update();
+            if (i->first > _now)
+              return i->first - _now;
+          }
+          task t;
+          t.swap(i->second);
+          _tasks.erase(i);
+          t();
+        }
+        return 0u;
+      }
+
+    protected:
+      void update() { _now = std::time(0); }
+
+    private:
+      time_t            _now;
+      task_queue        _tasks;
+    };
+
+    // I/O Event Dispatcher.
+
+    class scheduler : public core
+    {
+    public:
+      typedef int socket_id;
+
+      explicit scheduler(unsigned int size_hint = 512u) : _epoll_fd( epoll_create(size_hint) )
+      {
+        if (_epoll_fd < 0) throw system_error(errno, errno_ecat, "epoll_create(2) failed");
+      }
+
+      ~scheduler() { ::close(_epoll_fd); }
+
       void run()
       {
-        for (;;)
-        {
-          if (_tasks.empty())
-          {
-            if (_handlers.empty()) break;
-            run_epoll(-1);
-            continue;
-          }
-          else
-          {
-            task_queue::iterator const i( _tasks.begin() );
-            if (i->first > _now)
-            {
-              update();
-              if (i->first > _now)
-              {
-                run_epoll(static_cast<int>(i->first - _now) * 1000);
-                continue;
-              }
-            }
-            task t;
-            t.swap(i->second);
-            _tasks.erase(i);
-            t();
-          }
-        }
+        for (second_t next_event( core::run() ); !_handlers.empty(); next_event = core::run())
+          run_epoll(next_event ? static_cast<int>(next_event) * 1000 : -1);
       }
 
       void register_socket(socket_id const & s)
@@ -171,11 +182,6 @@ namespace postmaster
         }
       }
 
-      void update() { _now = std::time(0); }
-
-      time_t            _now;
-      task_queue        _tasks;
-
       typedef std::pair<task,task>        handler;
       typedef std::map<socket_id,handler> handler_map;
 
@@ -185,39 +191,124 @@ namespace postmaster
 
     // Basic I/O interface.
 
+    class system_socket;
+    typedef boost::shared_ptr<system_socket> socket;
+
     class system_socket : private boost::noncopyable
     {
     public:
-      explicit system_socket(scheduler & io, int fd) : _io(io), _sock(fd), _close_on_destruction(true)
-      {
-        BOOST_ASSERT(fd >= 0);
-      }
+      typedef boost::function1<void, socket> handler;
 
       ~system_socket()
       {
+        _io.unregister_socket(_sock);
         if (_close_on_destruction) ::close(_sock);
       }
 
-      size_t read(error_code &, void *, size_t);
-      size_t write(error_code &, void const *, size_t);
+      void close_on_destruction(bool b) { _close_on_destruction = b; }
 
-      size_t readv(error_code &, iovec const *, size_t);
-      size_t writev(error_code &, iovec const *, size_t);
+      char * recv(char *, char *);
+      char * send(char *, char *);
+      char const * send(char const *, char const *);
 
-    protected:
-      scheduler &       _io;
-      int const         _sock;
-      bool              _close_on_destruction;
+      friend socket create_socket(scheduler & io, int fd)
+      {
+        socket s;
+        s.reset( new system_socket(io, fd) );
+        return s;
+      }
+
+      friend void on_input(socket s, handler f, second_t timeout = 0, handler h = handler())
+      {
+        s->_input.set(s, f, timeout, h, &scheduler::on_input);
+      }
+
+      friend void on_output(socket s, handler f, second_t timeout = 0, handler h = handler())
+      {
+        s->_output.set(s, f, timeout, h, &scheduler::on_output);
+      }
+
+    private:
+      system_socket(scheduler & io, int fd) : _io(io), _sock(fd), _close_on_destruction(true)
+      {
+        BOOST_ASSERT(fd >= 0);
+        _io.register_socket(_sock);
+      }
+
+      scheduler &               _io;
+      int const                 _sock;
+      bool                      _close_on_destruction;
+
+      typedef void (scheduler::*registrar)(scheduler::socket_id, scheduler::task);
+
+      struct context
+      {
+        handler                   f;
+        handler                   h;
+        scheduler::task_id        timeout_id;
+        second_t                  timeout;
+
+        void set(socket s, handler f_, second_t to, handler h_, registrar on_event)
+        {
+          system_socket & self( *s );
+          self._io.cancel(timeout_id);
+          timeout_id = scheduler::task_id();
+          f.swap(f_);
+          h.swap(h_);
+          timeout = to;
+          if (f)
+          {
+            if (h && timeout)
+              timeout_id = self._io.schedule(boost::bind(handle_timeout, s, on_event), timeout);
+            (self._io.*on_event)(self._sock, boost::bind(handle_event, s, on_event));
+          }
+          else
+            (self._io.*on_event)(self._sock, scheduler::task());
+        }
+      };
+
+      context _input, _output;
+
+      static void handle_event(socket s, registrar on_event)
+      {
+        system_socket & self( *s );
+        context & ctx( on_event == &scheduler::on_input ? self._input : self._output );
+        self._io.cancel(ctx.timeout_id);
+        if (ctx.f) ctx.f(s);
+        if (ctx.f)
+        {
+          if (ctx.h && ctx.timeout)
+            ctx.timeout_id = self._io.schedule(boost::bind(handle_timeout, s, on_event), ctx.timeout);
+        }
+        else
+          (self._io.*on_event)(self._sock, scheduler::task());
+      }
+
+      static void handle_timeout(socket s, registrar on_event)
+      {
+        system_socket & self( *s );
+        context & ctx( on_event == &scheduler::on_input ? self._input : self._output );
+        handler f, h;
+        f.swap(ctx.f);
+        h.swap(ctx.h);
+        (self._io.*on_event)(self._sock, scheduler::task());
+        if (h) h(s);
+      }
     };
 
-    struct weak_socket : public system_socket
+    class echo_handler
     {
-      explicit weak_socket(scheduler & io, int fd) : system_socket(io, fd) { _close_on_destruction = false; }
+    public:
     };
   }
 }
 
 void print_id(unsigned int id) { std::cout << "task id " << id << std::endl; }
+
+void socket_timeout(postmaster::io::socket s)
+{
+  std::cout << "timeout on socket " << s << std::endl;
+}
 
 int main(int, char**)
 {
@@ -225,16 +316,22 @@ int main(int, char**)
   using namespace postmaster::io;
   using boost::bind;
 
-  scheduler io;
+  static scheduler io;
 
-  io.register_socket(STDIN_FILENO);
-  io.schedule(bind(&scheduler::unregister_socket, &io, STDIN_FILENO), 5u);
-  io.on_input(STDIN_FILENO, bind(print_id, 10u));
+  socket s( create_socket(io, STDIN_FILENO) ); s->close_on_destruction(false);
+  on_input(s, bind(print_id, 10u), 5u, bind(socket_timeout, s));
+  s.reset();
 
-  io.register_socket(STDOUT_FILENO);
-  io.schedule(bind(&scheduler::unregister_socket, &io, STDOUT_FILENO), 2u);
-  io.on_output(STDOUT_FILENO, bind(print_id, 20u));
 
+//   socket sout( create_socket(io, STDOUT_FILENO) ); sout->close_on_destruction(false);
+//   socket serr( create_socket(io, STDERR_FILENO) ); serr->close_on_destruction(false);
+
+//   io.schedule(bind(&boost::shared_ptr<system_socket>::reset, boost::ref(sin)), 5u);
+//   io.on_input(STDIN_FILENO, bind(print_id, 10u));
+//
+//   io.schedule(bind(&scheduler::unregister_socket, &io, STDOUT_FILENO), 2u);
+//   io.on_output(STDOUT_FILENO, bind(print_id, 20u));
+//
 
   io.schedule(bind(print_id, 3u), 1u);
   io.schedule(bind(print_id, 0u));
