ee7309d3da498b691d0e0cb050daebe0a5369cd1
Peter Simons <simons@cryp.to>
2007-11-23 03:31:54 +0100

    cosmetic

--- src/signal_block.hpp
+++ src/signal_block.hpp
@@ -1,6 +1,6 @@
 class signal_block : private boost::noncopyable
 {
-  sigset_t  _sigset;
+  sigset_t  _orig_set;
 
 public:
   signal_block()
@@ -8,12 +8,12 @@ public:
     sigset_t all;
     if (!sigfillset(&all))
       throw system_error(errno, errno_ecat, "signal_block: sigfillset(3) failed");
-    if (!sigprocmask(SIG_BLOCK, &all, &_sigset))
+    if (!sigprocmask(SIG_BLOCK, &all, &_orig_set))
       throw system_error(errno, errno_ecat, "signal_block: sigprocmask(2) failed");
   }
   ~signal_block()
   {
-    if (!sigprocmask(SIG_SETMASK, &_sigset, 0))
+    if (!sigprocmask(SIG_SETMASK, &_orig_set, 0))
       throw system_error(errno, errno_ecat, "~signal_block: sigprocmask(2) failed");
   }
 };
