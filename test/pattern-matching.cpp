6ea10e45cc87ec72eaeb7cce2d4a80b515c2b393
Peter Simons <simons@cryp.to>
2007-12-03 01:51:29 +0100

    dropped obsolete copyright headers

--- test/pattern-matching.cpp
+++ test/pattern-matching.cpp
@@ -1,15 +1,3 @@
-/*
- * Copyright (c) 2007 Peter Simons <simons@cryp.to>
- *
- * This software is provided 'as-is', without any express or
- * implied warranty. In no event will the authors be held liable
- * for any damages arising from the use of this software.
- *
- * Copying and distribution of this file, with or without
- * modification, are permitted in any medium without royalty
- * provided the copyright notice and this notice are preserved.
- */
-
 #include "postmaster.hpp"
 
 #define BOOST_AUTO_TEST_MAIN
